7d351ccbb89f9d7fcc74bcd1c2c41add575e1a0a
SimisaraJohnson <simisara.johnson@fingent.com>
2021-03-03 14:50:15 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f git@github.com:SimisaraJohnson/simi-b4ll/.git master:main
+git push -f git@github.com:SimisaraJohnson/simi-b4ll/.git 
 
 cd -
